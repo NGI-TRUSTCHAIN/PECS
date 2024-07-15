ede7720f5d905ec76a4ffed3557b12f04953c73d
Mirko Mangano <mangano_mirko@virgilio.it>
2024-07-15 11:03:11 +0200

    Refactoring PolicyBuilder Component

--- pecsi/policy_engine/src/main/java/com/pecs/pecsi/PolicyBuilder.java
+++ pecsi/policy_engine/src/main/java/com/pecs/pecsi/PolicyBuilder.java
@@ -14,6 +14,7 @@ public class PolicyBuilder {
         StringBuilder policy = new StringBuilder();
 
         Map<String, Boolean> globals = prefs.getPreferences().getGlobals().getPrefs();
+        Map<String, Boolean> enginePrefs = prefs.getPreferences().getEngineDataPrefs();
         List<String> dataList = Permissions.getDataList();
         List<String> engineDataList = Permissions.getEngineDataList();
 
@@ -44,6 +45,23 @@ public class PolicyBuilder {
             policy.append("<AttributeDesignator Category=\"urn:oasis:names:tc:xacml:3.0:attribute-category:action\" AttributeId=\"urn:oasis:names:tc:xacml:1.0:action:action-id\" DataType=\"http://www.w3.org/2001/XMLSchema#string\"/>\n\t\t\t\t\t");
             policy.append("</Match>\n\t\t\t\t</AllOf>\n\t\t\t</AnyOf>\n\t\t</Target>\n\t");
             policy.append("</Rule>\n\n\t");
+        }
+
+        for (String data : engineDataList) {
+            policy.append("\t<Rule RuleId=\"engine-");
+            policy.append(data);
+            policy.append("\" Effect=");
+            
+            if (enginePrefs.get(data) == true) policy.append("\"Permit\">");
+            else policy.append("\"Deny\">");
+
+            policy.append("\n\t\t");
+            policy.append("<Target>\n\t\t\t<AnyOf>\n\t\t\t\t<AllOf>\n\t\t\t\t\t");
+            policy.append("<Match MatchId=\"urn:oasis:names:tc:xacml:1.0:function:string-equal\">\n\t\t\t\t\t\t");
+            policy.append("<AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#string\">" + data + "</AttributeValue>\n\t\t\t\t\t\t");
+            policy.append("<AttributeDesignator Category=\"urn:oasis:names:tc:xacml:3.0:attribute-category:action\" AttributeId=\"urn:oasis:names:tc:xacml:1.0:action:action-id\" DataType=\"http://www.w3.org/2001/XMLSchema#string\"/>\n\t\t\t\t\t");
+            policy.append("</Match>\n\t\t\t\t</AllOf>\n\t\t\t</AnyOf>\n\t\t</Target>\n\t");
+            policy.append("</Rule>\n\n");
         }
 
         policy.append("</Policy>\n");
